abc0f1a583214e68bec8322ce0f965b8dbc725c6
colin.vokt <colin.vokt@CLC-LAB209-22.TCSD.tooelesd.org>
2018-09-04 13:18:52 -0600

    first commit

---
+++ src/colinvokt/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * hello world program print hello world in console
+ */
+package colinvokt.helloworld;
+
+/**
+ * @author colin.vokt
+ *9/18
+ *hello world
+ */
+public class HelloWorld {
+
+	public HelloWorld() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello World'");
+	}
+
+}
